a5b73523c37a26274fa36431200030baae182c90
Philopater Hany <86812518+PhilopaterHany@users.noreply.github.com>
2021-09-27 17:31:39 +0200

    Add files via upload

---
+++ js/master.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("to-top");
+window.onscroll = () => {
+    window.scrollY >= 400
+        ? (btn.style.right = "20px")
+        : (btn.style.right = "-1000px");
+};
+btn.onclick = () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+};
